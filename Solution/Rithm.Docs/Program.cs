ea73486f2f5b98ece0c07adf6dc7bbeb29fa3706
Rob Mackenzie <rob@jeraconcepts.com>
2023-03-31 06:50:36 -0400

    Added base component with cancellationtokensource

--- Solution/Rithm.Docs/Program.cs
+++ Solution/Rithm.Docs/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Rithm;
 using Rithm.Docs;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddRithm();
+
 
 await builder.Build().RunAsync();
