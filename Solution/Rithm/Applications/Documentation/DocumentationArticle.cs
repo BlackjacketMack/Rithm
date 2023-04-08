0fd1109195afeccd13013f4e75de48f6761c5f0a
Rob Mackenzie <rob@jeraconcepts.com>
2023-04-08 09:47:32 -0400

    Documentation article

---
+++ Solution/Rithm/Applications/Documentation/DocumentationArticle.cs
@@ -0,0 +1,24 @@
+﻿using Rithm;
+
+namespace Rithm.Documentation
+{
+    public class DocumentationArticle : MarkdownArticle, IArticleImage
+    {
+        public sealed override string Kind { get; set; } = "Docs";
+
+        public string? Image { get; set; }
+
+        /// <summary>
+        /// Draft sets the major version to 0.
+        /// </summary>
+        public bool Draft
+        {
+            get => base.Version.Major < 1;
+            set{
+                if(value)
+                    base.Version = new Version(0,base.Version.Minor,base.Version.Build,base.Version.Revision);
+                
+            }
+        }
+    }
+}
