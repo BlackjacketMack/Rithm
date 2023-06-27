ccd7ad56072f4223a54abe6fa9628ddc5422c575
Rob Mackenzie <rob@jeraconcepts.com>
2023-06-27 08:14:24 -0400

    Added generic blogarticle type extension.

--- Solution/Rithm/Extensions/RithmOptionsExtensions.cs
+++ Solution/Rithm/Extensions/RithmOptionsExtensions.cs
@@ -14,6 +14,14 @@ namespace Rithm
             return options;
         }
 
+        public static RithmOptions AddBlog<T>(this RithmOptions options) where T : BlogArticle
+        {
+            options.AddIngestor<EmbeddedMarkdownIngestor>(ing => ing.WithType<T>());
+            options.ServiceCollection.AddScoped<IBlogHelper, BlogHelper>();
+
+            return options;
+        }
+
         public static RithmOptions AddDocumentation(this RithmOptions options)
         {
             options.AddIngestor<EmbeddedMarkdownIngestor>(ing => ing.WithType<DocumentationArticle>());
