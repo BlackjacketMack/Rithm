fe36b6bf420df4c2abf886524d6bcde333e277c5
Rob Mackenzie <rob@jeraconcepts.com>
2023-07-17 07:26:29 -0400

    ArticleImage cleanup

--- Solution/Rithm/Models/ArticleImage.cs
+++ Solution/Rithm/Models/ArticleImage.cs
@@ -7,15 +7,4 @@ public class ArticleImage
     public string? Url { get; set; }
     public string? Credit { get; set; }
     public string? Caption { get; set; }
-
-
-    /// <summary>
-    /// An image can be set by a simple string url
-    /// </summary>
-    /// <param name="url"></param>
-    //public static implicit operator ArticleImage?(string url) => url == null ? null : new ArticleImage { Url = url };
-    //public static implicit operator string?(ArticleImage? image) => image?.Url;
-
-    //public static explicit operator ArticleImage?(string url) => url == null ? null : new ArticleImage(url);
-    //public static explicit operator string?(ArticleImage? image) => image?.Url;
 }
